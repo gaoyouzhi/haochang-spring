1b7cceac825904bbeb7420510f80b3e1b0e9f4c2
youzhi.gao <youzhi.gao@ikang.com>
2020-12-02 16:07:37 +0800

    haochang-spring  各框架整合

---
+++ haochang-spring-boot/src/main/java/com/haochang/boot/HaoChangSpringBootApplication.java
@@ -0,0 +1,17 @@
+package com.haochang.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description: 描述：启动类
+ * @author: youzhi.gao
+ * @date: 2020-12-02 16:02
+ */
+@SpringBootApplication
+public class HaoChangSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaoChangSpringBootApplication.class, args);
+    }
+}
